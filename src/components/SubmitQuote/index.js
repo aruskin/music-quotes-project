37a0cf4b7f2f19fe7d77722d14c9b8db2e82119e
aruskin <asruskin@gmail.com>
2021-12-07 18:21:59 -0500

    Limited working version of quote submission (does not include speaker or subject yet)

--- src/components/SubmitQuote/index.js
+++ src/components/SubmitQuote/index.js
@@ -2,14 +2,20 @@ import React, {useEffect, useState} from "react";
 import NavigationSidebar from "../NavigationSidebar";
 import userService from '../../services/user-service';
 import musicbrainzService from '../../services/musicbrainz-service';
+import {submitQuote} from '../../services/quote-service';
 
 
 function QuoteForm(){
-    const [speaker, setSpeaker] = useState({keyword: '', selectOptions: []})
-    const [subjects, setSubjects] = useState([{keyword: '', selectOptions: []}])
+    const [speaker, setSpeaker] = useState({keyword: '', selectOptions: [], selected: null});
+    const [subjects, setSubjects] = useState([{keyword: '', selectOptions: [], selected: null}]);
+    let [quote, setQuote] = useState({
+        text: '',
+        source: null,
+        sourceDate: null,
+        sourceURL: null});
 
     function handleSpeakerChange(event){
-        return(setSpeaker({keyword: event.target.value, selectOptions: []}))
+        return(setSpeaker({keyword: event.target.value, selectOptions: [], selected: null}))
     }
 
     async function handleSpeakerSearch(event){
@@ -18,7 +24,7 @@ function QuoteForm(){
         return(setSpeaker(oldValues => ({...oldValues, selectOptions: artistResults})))
     }
 
-    function renderSelectOptions(myState, selectId, index=null){
+    function renderSelectOptions(myState, setMyState, selectId, index=null){
         var items;
         var artists;
         console.log(index);
@@ -34,7 +40,23 @@ function QuoteForm(){
         }
         return(
             <div className="form-group col">
-                <select className="form-control" id={selectId}>
+                <select
+                    className="form-control"
+                    id={selectId}
+                    required
+                    value={index !== null ? myState[index].selected : myState.selected}
+                    onChange={(event) => {
+                        if(index !== null){
+                            setMyState(oldValues =>
+                                [...oldValues.slice(0, index),
+                                {...oldValues[index], selected: event.target.value},
+                                ...oldValues.slice(index+1)
+                                ])
+                        }
+                        else{
+                            setMyState(oldValues => ({...oldValues, selected: event.target.value}))
+                        }
+                    }}>
                     <option value="" disabled selected>Select the artist</option>
                     {items}
                 </select>
@@ -47,7 +69,7 @@ function QuoteForm(){
             console.log(event.target.value)
             return(setSubjects(oldValues =>
                 [...oldValues.slice(0, index),
-                {keyword: event.target.value, selectOptions: []},
+                {keyword: event.target.value, selectOptions: [], selected: null},
                 ...oldValues.slice(index+1)
                 ]))
          }
@@ -59,7 +81,7 @@ function QuoteForm(){
             var currentSubjects = subjects;
             return(setSubjects(oldValues =>
                  [...oldValues.slice(0, index),
-                 {keyword: artist, selectOptions: artistResults},
+                 {keyword: artist, selectOptions: artistResults, selected: null},
                  ...oldValues.slice(index+1)
                  ]
             ))
@@ -77,22 +99,41 @@ function QuoteForm(){
                     className="btn btn-secondary col"
                     onClick={handleSubjectSearch}>Search
                 </button>
-                {renderSelectOptions(subjects, `subjectSelect`+index, index)}
+                {renderSelectOptions(subjects, setSubjects, `subjectSelect`+index, index)}
             </div>
         )
     }
 
     function appendInput(event){
-        return(setSubjects(oldValues => [...oldValues, {keyword: "", selectOptions: []}]))
+        return(setSubjects(oldValues => [...oldValues,
+            {keyword: "", selectOptions: [], selected: null}]))
     }
 
-    let handleSubmit = (event) => {}
+    function handleSubmit(event){
+        event.preventDefault();
+        const body = {
+            ...quote,
+            speaker: speaker.selected,
+            subject: subjects.map((item, index) => item.selected)
+        }
+        console.log(body)
+        return(submitQuote(quote))
+    }
 
     return(
     <form onSubmit={handleSubmit} className="me-2">
         <div className="form-group">
             <label htmlFor="quoteText">Quote*</label>
-            <textarea className="form-control" id="quoteText" rows="3" required></textarea>
+            <textarea
+                className="form-control"
+                id="quoteText"
+                rows="3"
+                required
+                value={quote.text}
+                onChange={
+                    (event) =>
+                        setQuote(oldValues => ({...oldValues, text: event.target.value}))}>
+            </textarea>
         </div>
         <div className="form-group">
             <label htmlFor="quoteSource">Source*</label>
@@ -100,20 +141,29 @@ function QuoteForm(){
                 id="quoteSource"
                 placeholder="e.g., NME"
                 type="text"
-                required/>
+                required
+                value={quote.source}
+                onChange={(event) =>
+                    setQuote(oldValues => ({...oldValues, source: event.target.value}))}/>
         </div>
         <div className="form-group">
             <label htmlFor="quoteDate">Source Date</label>
             <input className="form-control"
                 type="date"
-                id="quoteDate"/>
+                id="quoteDate"
+                value={quote.sourceDate}
+                onChange={(event) =>
+                    setQuote(oldValues => ({...oldValues, sourceDate: event.target.value}))}/>
         </div>
         <div className="form-group">
             <label htmlFor="quoteURL">Source URL</label>
             <input className="form-control"
             type="url"
             id="quoteURL"
-            placeholder="http://www.example.com/noel-gallagher-interview"/>
+            placeholder="http://www.example.com/noel-gallagher-interview"
+            value={quote.sourceURL}
+            onChange={(event) =>
+                setQuote(oldValues => ({...oldValues, sourceURL: event.target.value}))}/>
         </div>
         <label htmlFor="speakerGroup">Speaker*</label>
         <div className="row align-items-center" id="speakerGroup">
@@ -129,7 +179,7 @@ function QuoteForm(){
             <button
                 className="btn btn-secondary col"
                 onClick={handleSpeakerSearch}>Search</button>
-            {renderSelectOptions(speaker, `speakerSelect`)}
+            {renderSelectOptions(speaker, setSpeaker, `speakerSelect`)}
         </div>
         <label htmlFor="subjectGroup">Subject</label>
         <div className="row align-items-center" id="subjectGroup">
--- src/services/quote-service.js
+++ src/services/quote-service.js
@@ -1,9 +1,18 @@
 const QUOTE_API = "http://localhost:4000/api/quotes";
 
-const findAllQuotes = (dispatch) =>
+export const findAllQuotes = () =>
     fetch(QUOTE_API)
         .then(response => response.json());
 
+export const submitQuote = (quote) =>
+    fetch(QUOTE_API, {
+        method: 'POST',
+        body: JSON.stringify(quote),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+
 export default {
-    findAllQuotes
+    findAllQuotes, submitQuote
 }
