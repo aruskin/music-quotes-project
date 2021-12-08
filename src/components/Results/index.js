b0760d791a5ee17801b4f8e68696f379aa8e9211
aruskin <asruskin@gmail.com>
2021-12-08 18:42:02 -0500

    Reformat results list to table

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -23,15 +23,39 @@ function Results({loggedIn=false}) {
            items = artists
              .map(function(item, index) {
                var detailsURL = "/details/" + item.id
-               return <li className="list-group-item"
-                       key={index}>
-                       <Link to={detailsURL}>{item.name}</Link> <i>{item.disambiguation}</i> {item.country}</li>;
-             });
+               return(
+                   <tr>
+                        <th scope="row">{index + 1}</th>
+                        <th>{item.name}</th>
+                        <th>{item.disambiguation}</th>
+                        <th>{item.country}</th>
+                        <th></th>
+                        <th></th>
+                        <th><Link to={detailsURL} type="button" className="btn btn-link">See Details</Link></th>
+                   </tr>)
+                 });
           }
         return (
           <div>
           Found {numItems} results in Musicbrainz
-          <ul className="list-group">{items}</ul>
+          <div class="table-responsive-sm">
+          <table className="table table-striped table-sm">
+              <thead className="thead-dark">
+                  <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Disambiguation</th>
+                      <th scope="col">Country</th>
+                      <th scope="col">Quotes From</th>
+                      <th scope="col">Quotes About</th>
+                      <th scope="col"></th>
+                  </tr>
+              </thead>
+              <tbody>
+              {items}
+              </tbody>
+          </table>
+          </div>
           </div>
        )
     }
