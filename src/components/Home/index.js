9a0edda8fbc43f14f82e15d6342f1aaaa33dae00
aruskin <asruskin@gmail.com>
2021-12-12 09:40:42 -0500

    Add more specific info to homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -21,24 +21,25 @@ function Home(){
                         <NavigationSidebar active="home"/>
             </div>
             <div className="col-10">
-            <h1>Home</h1>
-            {user.username ? <div>Welcome back, {user.username}!</div> : ''}
+            <h1>Rock Insults</h1>
+            <p>A crowdsourced database of musicians talking shit about each other</p>
             <h2>Project requirements</h2>
             <ol>
                 <li>Must be mapped to either the root context ("/") or ("/home").</li>
                 <li>Must be the first page when visiting the website</li>
-                <li style={{background: 'yellow'}}>Must display generic content for anonymous users. The content must be dynamic based on the latest data. For instance, you might display snippets and links to the most recent post, review, or member who recently joined</li>
+                <li>Must display generic content for anonymous users. The content must be dynamic based on the latest data. For instance, you might display snippets and links to the most recent post, review, or member who recently joined</li>
                 <li style={{background: 'yellow'}}>Must display specific content for the logged in user. The content must be dynamic based on the most recent data entered by the logged in user. For instance, you might display snippets and links to the most recent post or review created by the logged in user</li>
                 <li style={{background: 'yellow'}}>Must be clear to what the Web site is about and must look polished and finished</li>
             </ol>
             <h2>Most recently submitted quotes</h2>
-                <ul className="list-group">
-                    {
-                        quotes.map(quote =>
-                            <Quote quote={quote} key={quote._id}/>
-                        )
-                    }
-                </ul>
+            <ul className="list-group">
+                {
+                    quotes.map(quote =>
+                        <Quote quote={quote} key={quote._id}/>
+                    )
+                }
+            </ul>
+            {user.username ? <h2>Specific content for {user.username}</h2> : ''}
             </div>
         </div>
     )
