5ad82b5c7b20e2824f639c38489a883c9039788c
aruskin <asruskin@gmail.com>
2021-12-14 18:57:59 -0500

    Write minimal privacy policy

--- src/components/PrivacyPolicy/index.js
+++ src/components/PrivacyPolicy/index.js
@@ -9,12 +9,20 @@ function PrivacyPolicy(){
             </div>
             <div className="col-10">
                 <h1>Privacy Policy</h1>
-                <h2>Project requirements</h2>
-                <ol>
-                    <li style={{background: 'yellow'}}>Must be made available to your users when they first visit your website.</li>
-                    <li style={{background: 'yellow'}}>Must be easily available for subsequent review.</li>
-                    <li style={{background: 'yellow'}}>Must not employ any “dark patterns” for attaining user consent.</li>
-                </ol>
+                <h3>What We Collect</h3>
+                <h4>Information You Provide</h4>
+                <p>Account information: We only ask (non-admin) users to provide a username and password. Your username will be public.</p>
+                <p>Submissions: We collect the quotes that you submit and associate them with your account.</p>
+                <h4>Information We Collect Automatically</h4>
+                <p>We keep a record of when your account was created and when you submitted quotes and make those public.</p>
+                <h3>Why Are We Collecting This Information</h3>
+                <p>We require users to register to submit quotes in order to discourage vandalism. We keep track of the date your account was created and the quotes you've submitted to help us weed out bad actors.</p>
+                <h3>Who Will Have Access to This Information</h3>
+                <p>Your username, account creation date, role, and submitted quotes will be visible to all users of the site. The database administrator will have access to your passwords.</p>
+                <h3>How Are We Protecting and Storing the Information</h3>
+                <p>All the user information we collect is stored in a MongoDB database. Sensitive information will not be encrypted. While some of this information is accessible via a web API, your password should only be accessible to you and the database administrator.</p>
+                <h3>What Control Do You Have Over Your Data</h3>
+                <p>You can change your password whenever you want. At the moment, you cannot change your username or delete your account or submitted quotes. Only administrators may delete submitted quotes.</p>
             </div>
         </div>
     )
